112d6202cbc94f09cdb31398af189201cf1f9ab6
Matthieu Mourisson <mareek@gmail.com>
2016-02-02 00:08:41 +0100

    Added settings for multiplication

---
+++ JapprendACompter/Config.cs
@@ -0,0 +1,18 @@
+﻿using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace JapprendACompter
+{
+    public static class Config
+    {
+        private const string MultiplicationMaxSetting = "MultiplicationMax";
+
+        private static IPropertySet SettingValues { get { return ApplicationData.Current.RoamingSettings.Values; } }
+
+        public static int MultiplicationMax
+        {
+            get { return (int?)SettingValues[MultiplicationMaxSetting] ?? 5; }
+            set { SettingValues[MultiplicationMaxSetting] = value; }
+        }
+    }
+}
--- JapprendACompter/Multiplication.cs
+++ JapprendACompter/Multiplication.cs
@@ -9,7 +9,7 @@ namespace JapprendACompter
         protected override Operation GenerateOperation()
         {
             var rnd = new Random();
-            var leftOperand = rnd.Next(5) + 1;
+            var leftOperand = rnd.Next(Config.MultiplicationMax) + 1;
             var rightOperand = rnd.Next(9) + 1;
 
             return new Operation(leftOperand, rightOperand, leftOperand * rightOperand, "x");
